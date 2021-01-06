4866ed780f6d2d509448853666b0b00613f345cc
itizawa <itizawa@weseek.co.jp>
2021-01-06 17:27:25 +0000

    adjust preview

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,1 +1,13 @@
 import '!style-loader!css-loader!sass-loader!../src/styles/styles.scss';
+import { configure, addDecorator } from "@storybook/react";
+import React from "react";
+import '~/styles/styles.scss';
+
+addDecorator((storyFn) => (
+  <>
+    {storyFn()}
+  </>
+));
+
+const req = require.context("../src", true, /\.stories\.tsx$/);
+configure(req, module);
